af3d770389c5a06f03c40094af03c2ac1f6b3a01
moshedeitsch <41856538+moshedeitsch@users.noreply.github.com>
2019-05-02 18:05:42 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+$a = array(
+    "אלון",
+     "משה",
+      "דוד",
+      "יוסף",
+      "גבי",
+
+    ); 
+
+
+
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+if ($q !== "") {
+  $q = strtolower($q);
+  $len=strlen($q);
+  foreach($a as $name) {
+    if (stristr($q, substr($name, 0, $len))) {
+      if ($hint === "") {
+        $hint = $name;
+      } else {
+        $hint .= ", $name";
+      }
+    }
+  }
+}
+
+echo $hint === "" ? "no suggestion" : $hint;
+?> 
